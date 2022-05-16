7c65e1bb5c11fc95c3747f1e50295fef7370cdcb
Sunghyun Cho <mail@chosunghyun.com>
2022-05-16 22:14:47 +0900

    added default values

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Hello World</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured
+            content or information.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+</style>
