22e327be73a3db077a264a79c469616084fba88a
Sunghyun Cho <mail@chosunghyun.com>
2022-05-16 22:10:36 +0900

    organize files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,4 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   buildModules: ['@nuxtjs/svg', '@nuxtjs/style-resources'],
-  styleResources: {
-    scss: ['~/assets/styles/tokens.scss']
-  },
 })
